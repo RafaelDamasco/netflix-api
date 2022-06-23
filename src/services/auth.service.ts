6b9221e62c759dc8066970940bc380489a6ba4d0
RafaelDamasco <rafaeldamasco_@hotmail.com>
2022-06-23 08:34:52 -0300

    feature: add env

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,14 +1,20 @@
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 import UnauthorizedException from "../exceptions/unauthorized.exception"
 import UserService from "./user.service"
 
 class AuthService {
+  /**
+   * Realiza autenticação do usuário
+   *
+   * @param email email do usuário
+   * @param password email do usuário
+   * @returns LoginResponse
+   *
+   */
   async login(email: string, password: string) {
-    // TODO: Verificar no banco de dados o email
-    // Todo: Caso o usuário exista, encryptar o password e verificar se bate
-    // TODO: criar o token caso o usuário exista e o password esteja correto
     const userService = new UserService()
+    const secret = process.env.SECRET || ""
 
     const user = await userService.getUserByEmail(email)
 
@@ -21,12 +27,12 @@ class AuthService {
     if (!passwordMatches) {
       throw new UnauthorizedException()
     }
-    // Cria o token
+
     const token = jwt.sign({
       sub: user.id,
       iat: Date.now(),
       email: user.email
-    }, 'ABCBANANA')
+    }, secret)
 
     return {
       token
